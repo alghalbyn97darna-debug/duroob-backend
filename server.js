713753951a1f71428c2ab53fe36e3c78f3123a19
alghalbyn97darna <alghalbyn97darna@gmail.com>
2026-08-23 13:53:03 +0200

    أول نسخة من سيرفر دروب المستقبل

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const trips = [
+  { id: 1, from: "طرابلس", to: "مصراتة", time: "08:00 ص", price: 25, company: "شركة الوحدة" },
+  { id: 2, from: "طرابلس", to: "بنغازي", time: "09:30 ص", price: 60, company: "شركة الصداقة" },
+  { id: 3, from: "مصراتة", to: "سرت", time: "11:00 ص", price: 20, company: "شركة الوحدة" },
+  { id: 4, from: "بنغازي", to: "البيضاء", time: "01:00 م", price: 15, company: "شركة الشرق" }
+];
+
+app.get('/api/trips', (req, res) => {
+  res.json(trips);
+});
+
+app.get('/', (req, res) => {
+  res.send('سيرفر دروب المستقبل شغال ✅');
+});
+
+app.listen(PORT, () => {
+  console.log('السيرفر شغال على http://localhost:' + PORT);
+});
+
+app.use(express.static(__dirname));
